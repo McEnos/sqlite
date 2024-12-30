d9ba16f829e443fadb1332cf8b97e9a357fcf0bb
McEnos <joshuamcenos@gmail.com>
2024-12-30 07:54:10 +0300

    Added a simple REPL(Read Execute Print Loop)

--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 /**
  * https://www.lucavall.in/blog/how-to-structure-c-projects-my-experience-best-practices
@@ -9,8 +13,67 @@ typedef struct
     size_t buffer_length;
     ssize_t input_length;
 } InputBuffer;
-int main()
+
+InputBuffer *new_input_buffer()
 {
-    printf("Hello c programming language\n");
-    return 0;
+    InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
+    if (input_buffer == NULL)
+    {
+        printf("Error allocating memory for InputBuffer.\n");
+        exit(EXIT_FAILURE);
+    }
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+void print_prompt()
+{
+    printf("db > ");
+}
+/**
+ * Read user input into the buffer
+ */
+void read_input(InputBuffer *input_buffer)
+{
+    ssize_t bytes_read =
+        getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0)
+    {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+/**
+ * Free the InputBuffer and its allocated memory
+ */
+void close_input_buffer(InputBuffer *input_buffer)
+{
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+int main(int argc, char *argv[])
+{
+    InputBuffer *input_buffer = new_input_buffer();
+    while (true)
+    {
+        print_prompt();
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
+    }
 }
